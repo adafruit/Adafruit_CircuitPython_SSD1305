7ee9a184fd09575f99a18657d24e923debc956c7
Mikey Sklar <mikeysklar@gmail.com>
2025-03-21 20:02:04 -0700

    linting

--- adafruit_ssd1305.py
+++ adafruit_ssd1305.py
@@ -241,7 +241,7 @@ class SSD1305_I2C(_SSD1305):
             height,
             external_vcc=external_vcc,
             reset=reset,
-            col=col, # <-- Forwarded col parameter to base class
+            col=col,  # <-- Forwarded col parameter to base class
         )
 
     def write_cmd(self, cmd: int) -> None:
